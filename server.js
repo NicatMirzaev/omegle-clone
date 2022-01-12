7c7fc53d34f93c678a7acce358b9e98549aa9f94
NicatMirzaev <nicatmirzoev111@gmail.com>
2022-01-12 05:28:18 -0800

    implemented connection between socket and client, added app state

--- server.js
+++ server.js
@@ -2,10 +2,52 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require("socket.io")(server);
+const { v4: uuidv4 } = require('uuid');
 const port = process.env.PORT || 5000;
+
+let users = [];
 
 app.get('/health', (req, res) => {
   res.send("OK");
 });
+
+io.on("connection", socket => {
+  socket.on("new-user", () => {
+    const generatedUUID = uuidv4();
+    let remote_peer_id = null;
+
+    for(const user of users) {
+      if(user.started === true && user.remote_peer_id === null) {
+        remote_peer_id = user.peer_id;
+        console.log(user);
+        io.to(user.socket_id).emit("peer-found", { id: generatedUUID });
+        break;
+      }
+    }
+
+    const user = {
+      started: true,
+      peer_id: generatedUUID,
+      socket_id: socket.id,
+      remote_peer_id
+    }
+
+    users.push(user);
+
+
+    socket.emit("new-user", { peer_id: generatedUUID, remote_peer_id });
+
+    console.log(`A user connected (peer id: ${generatedUUID}`);
+  })
+  socket.on("disconnect", () => {
+    users = users.filter(user => {
+      if(user.socket_id === socket.id) {
+        console.log(`A user disconnected (peer id: ${user.peer_id}`);
+        return true;
+      }
+      return false;
+    })
+  })
+})
 
 server.listen(port, () => console.log(`🚀 Server is now running on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -1,34 +1,82 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { io } from "socket.io-client";
+
+let socket = null;
+
+function Loading() {
+  return (
+    <svg
+      className="spinner"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="#FFFFFF"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      />
+    </svg>
+  )
+}
 
 function App() {
   const myVideo = useRef();
+  const remoteVideo = useRef()
+  const [started, setStarted] = useState(null);
+  const [peerId, setPeerId] = useState(null);
+  const [remotePeerId, setRemotePeerId] = useState(null);
+  const [permission, setPermission] = useState(false);
 
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     .then(stream => {
       myVideo.current.srcObject = stream;
       myVideo.current.play();
+
+      setPermission(true);
+      socket = io();
+      socket.on("new-user", ({ peer_id, remote_peer_id }) => {
+        setStarted(true);
+        setPeerId(peer_id);
+        setRemotePeerId(remote_peer_id);
+      })
+      socket.on("peer-found", ({ id }) => {
+        setRemotePeerId(id);
+      })
     })
     .catch(error => {
       console.log(error);
+      setPermission(false);
     })
 
+    return () => socket.disconnect(); //cleanup
+
   }, [])
+
+  const onClickStart = () => {
+    if(!permission) return;
+
+    if(!started) {
+      socket.emit("new-user");
+    }
+  }
 
   return (
     <div className="container">
       <div className="leftSection">
-        <div className="my-video">
+        <div className="video">
           <video ref={myVideo} />
+          {!started && <span style={{textAlign: "center", color: "white"}}>To get started, please allow camera and microphone access. Then click to start button.</span>}
         </div>
         <div className="buttons">
-          <button className="btnNext">Next</button>
-          <button className="btnStop">Stop</button>
+          <button onClick={onClickStart} className="btnNext">{!started ? "Start" : "Next"}</button>
+          <button disabled={started} className="btnStop">Stop</button>
         </div>
       </div>
       <div className="rightSection">
-        <div className="my-video">
-          test
+        <div className="video">
+          <video ref={remoteVideo} />
+          {started && remotePeerId === null ? <Loading/> : null}
         </div>
       </div>
     </div>
